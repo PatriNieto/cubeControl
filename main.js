db62b53bba21e7e1f85bd6563752a42c5815c71c
Patricia Nieto <patricianieto2.0@gmail.com>
2025-01-31 13:53:53 +0100

    imports

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@latest/examples/jsm/controls/OrbitControls.js';
 
 //create  scene
 
